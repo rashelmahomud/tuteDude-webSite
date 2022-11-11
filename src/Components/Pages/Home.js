6e2e5add91ff55f4bcc5161e6fa352b9911ac397
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-11-11 23:39:39 +0600

    added user informations

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ExploreProgram from './ExploreProgram';
 import FullRefound from './FullRefound';
+import OurClient from './OurClient';
 
 const Home = () => {
     return (
         <div>
-            
+
             <FullRefound />
             <ExploreProgram />
+            <OurClient />
         </div>
     );
 };
---
+++ src/Components/Pages/OurClient.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const OurClient = () => {
+    return (
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 bg-fuchsia-400 px-48 p-10'>
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold'>21</h1>
+                <h3>courses</h3>
+            </div>
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold'>200+</h1>
+                <h3>hours content</h3>
+            </div>
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold'>700+</h1>
+                <h3>Students doubts solved</h3>
+            </div>
+
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold'>564$</h1>
+                <h3>return advantage</h3>
+            </div>
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl'>Highly Skilled &</h1>
+                <h3>Structured course</h3>
+            </div>
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold'>5 Min</h1>
+                <h3>doubt clearance</h3>
+            </div>
+
+        </div>
+    );
+};
+
+export default OurClient;
