4f75cd276a72d9004ed96ea3b75b04471fff9185
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-11-11 22:10:07 +0600

    added home page

--- src/Components/Pages/FullRefound.js
+++ src/Components/Pages/FullRefound.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import banner from '../../assect/fsd.png'
 
 const FullRefound = () => {
     return (
@@ -10,15 +11,17 @@ const FullRefound = () => {
             <div className='grid grid-cols-2'>
                 <div className='text-left '>
                     <p className='font-bold text-fuchsia-600'>The Only Programme With </p>
-                    <h1 className='text-4xl font-bold my-2 text-fuchsia-800'>FULL</h1>
-                    <h2 className='text-4xl font-bold text-fuchsia-800'>REFOUND</h2>
-                    <p className='my-2 font-bold text-fuchsia-600'>On Course Complete</p>
+                    <h1 className='text-5xl font-bold my-3 text-fuchsia-800'>FULL</h1>
+                    <h2 className='text-5xl font-bold text-fuchsia-800'>REFOUND</h2>
+                    <p className='my-3 font-bold text-fuchsia-600'>On Course Complete</p>
 
                     <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-500 text-white bg-gradient-to-r from-violet-700 to-violet-400">Know HOW?</button>
 
                 </div>
 
-                <div>hi</div>
+                <div>
+                    <img className='' src={banner} alt='side-baner' />
+                </div>
             </div>
             <div className='mt-10'>
                 <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-500 text-white bg-gradient-to-r from-violet-700 to-violet-400">How does it Work?</button>
