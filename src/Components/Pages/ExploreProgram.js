6c7b0d0582f652605bdb4eed3aaeee01d4a8cfcc
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-11-11 23:05:24 +0600

    added new features

--- src/Components/Pages/ExploreProgram.js
+++ src/Components/Pages/ExploreProgram.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import pcMen from '../../assect/photoshop1.jpg'
 
 const ExploreProgram = () => {
     return (
-        <div className='lg:px-24 px-5'>
-            <div className='my-10 bg-blue-500 grid grid-cols-2 bg-gradient-to-r from-violet-700 to-fuchsia-400 lg:p-16 rounded '>
+        <div className='lg:px-24 px-2'>
+            <div className='my-10 bg-blue-500 grid lg:grid-cols-2 grid-cols-1  bg-gradient-to-r from-violet-700 to-fuchsia-400 lg:p-16 rounded '>
 
-                <div className='text-left'>
-                    <h2 className='text-2xl text-white font-bold'>Brighten up Yourself with some Latest Skills</h2>
-                    <p className='text-white font-bold my-2'>Learn any skill at your own pace with personal mentorship and immediate doubt clearing by your own dude</p>
-                    <div className='mt-10'>
+                <div className='text-left p-3'>
+                    <h2 className='text-3xl text-white font-bold'>Brighten up Yourself with some Latest Skills</h2>
+                    <p className='text-white font-bold my-2'>Learn any skill at your own pace with personal mentorship and immediate doubt clearing by your own dude Hello personal mentorship and immediate doubt clearing by your own dude</p>
+                    <div className='mt-20'>
                         <button className="btn btn-outline bg-white rounded-none lg:mr-5 rounded border-2 border-fuchsia-800">Explore Programs</button>
 
                     </div>
 
                 </div>
-                <div>ji</div>
+                <div className='p-2'>
+                    <img className='w-72 rounded mx-auto' src={pcMen} alt='pc-men' />
+                </div>
             </div>
 
         </div>
--- src/Components/Shared/Navigation.js
+++ src/Components/Shared/Navigation.js
@@ -11,13 +11,13 @@ const Navigation = () => {
 
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-200">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu dropdown-content mt-3 p-2 w-52">
+                    <ul tabIndex={0} className="menu dropdown-content mt-3 p-2 w-52 font-sans text-violet-500">
                         {naivations}
                     </ul>
                 </div>
@@ -25,7 +25,7 @@ const Navigation = () => {
 
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
+                <ul className="menu menu-horizontal p-0 font-sans text-violet-800 font-bold">
 
                     {naivations}
                 </ul>
@@ -33,7 +33,7 @@ const Navigation = () => {
             <div className="navbar-end">
 
                 <button className="btn btn-outline rounded-none lg:mr-5">Login</button>
-                <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-500 text-white bg-gradient-to-r from-violet-300 to-violet-700">Sign Up</button>
+                <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-800 font-bold text-white bg-gradient-to-r from-violet-300 to-violet-700">Sign Up</button>
             </div>
         </div>
     );
