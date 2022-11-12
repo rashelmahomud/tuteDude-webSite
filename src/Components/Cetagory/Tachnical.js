a5cc46ad2a85f1f648d2c07842019389a614594d
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-11-12 16:30:12 +0600

    added new catagory

--- src/Components/Cetagory/Tachnical.js
+++ src/Components/Cetagory/Tachnical.js
@@ -1,37 +1,97 @@
 import React from 'react';
 import CountUp from 'react-countup';
-
+import img from '../../assect/icon/app (1).svg'
 
 const Tachnical = () => {
     return (
         <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-10 gap-5 bg-blue-400 lg:px-48 p-10'>
-        <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
-            <h1 className='text-3xl font-bold text-fuchsia-700'><CountUp delay={2} end={21} /></h1>
-            <h3 className='text-fuchsia-500 font-bold'>courses</h3>
-        </div>
-        <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
-            <h1 className='text-3xl font-bold text-fuchsia-700'><CountUp delay={2} end={200} />+</h1>
-            <h3 className='text-fuchsia-500 font-bold'> hours content</h3>
-        </div>
-        <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
-            <h1 className='text-3xl font-bold text-fuchsia-700'><CountUp delay={1} end={700} />+</h1>
-            <h3 className='text-fuchsia-500 font-bold'>Students doubts solved</h3>
-        </div>
 
-        <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
-            <h1 className='text-3xl font-bold text-fuchsia-700'><CountUp delay={1} end={456} />$</h1>
-            <h3 className='text-fuchsia-500 font-bold'>return advantage</h3>
-        </div>
-        <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
-            <h1 className='text-3xl text-fuchsia-700'>Highly Skilled &</h1>
-            <h3 className='text-fuchsia-500 font-bold'>Structured course</h3>
-        </div>
-        <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
-            <h1 className='text-3xl font-bold text-fuchsia-700'><CountUp delay={2} end={7} /> Min</h1>
-            <h3 className='text-fuchsia-500 font-bold'>doubt clearance</h3>
-        </div>
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold text-fuchsia-700 text-left'>Python</h1>
+               <div className='flex'>
+                <div className='text-left py-3'>
+                    <span><i class="fa-regular fa-clock text-blue-500"></i> 38 hours</span> < br/>
+                    <span><i class="fa-solid fa-video text-green-600"></i> 254 Lectures</span>
+                </div>
+                <div className='mx-auto'>
+                    <img className='w-24' src={img} alt='logo' />
+                </div>
+               </div>
+               <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-800 font-bold text-white bg-gradient-to-r from-violet-300 to-violet-700 rounded w-full">View Course</button>
+            </div>
 
-    </div>
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold text-fuchsia-700 text-left'>Python</h1>
+               <div className='flex'>
+                <div className='text-left py-3'>
+                    <span><i class="fa-regular fa-clock text-blue-500"></i> 38 hours</span> < br/>
+                    <span><i class="fa-solid fa-video text-green-600"></i> 254 Lectures</span>
+                </div>
+                <div className='mx-auto'>
+                    <img className='w-24' src={img} alt='logo' />
+                </div>
+               </div>
+               <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-800 font-bold text-white bg-gradient-to-r from-violet-300 to-violet-700 rounded w-full">View Course</button>
+            </div>
+
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold text-fuchsia-700 text-left'>Python</h1>
+               <div className='flex'>
+                <div className='text-left py-3'>
+                    <span><i class="fa-regular fa-clock text-blue-500"></i> 38 hours</span> < br/>
+                    <span><i class="fa-solid fa-video text-green-600"></i> 254 Lectures</span>
+                </div>
+                <div className='mx-auto'>
+                    <img className='w-24' src={img} alt='logo' />
+                </div>
+               </div>
+               <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-800 font-bold text-white bg-gradient-to-r from-violet-300 to-violet-700 rounded w-full">View Course</button>
+            </div>
+
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold text-fuchsia-700 text-left'>Python</h1>
+               <div className='flex'>
+                <div className='text-left py-3'>
+                    <span><i class="fa-regular fa-clock text-blue-500"></i> 38 hours</span> < br/>
+                    <span><i class="fa-solid fa-video text-green-600"></i> 254 Lectures</span>
+                </div>
+                <div className='mx-auto'>
+                    <img className='w-24' src={img} alt='logo' />
+                </div>
+               </div>
+               <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-800 font-bold text-white bg-gradient-to-r from-violet-300 to-violet-700 rounded w-full">View Course</button>
+            </div>
+
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold text-fuchsia-700 text-left'>Python</h1>
+               <div className='flex'>
+                <div className='text-left py-3'>
+                    <span><i class="fa-regular fa-clock text-blue-500"></i> 38 hours</span> < br/>
+                    <span><i class="fa-solid fa-video text-green-600"></i> 254 Lectures</span>
+                </div>
+                <div className='mx-auto'>
+                    <img className='w-24' src={img} alt='logo' />
+                </div>
+               </div>
+               <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-800 font-bold text-white bg-gradient-to-r from-violet-300 to-violet-700 rounded w-full">View Course</button>
+            </div>
+
+            <div className='bg-gradient-to-r from-slate-100 to-gray-300 p-5 rounded-md'>
+                <h1 className='text-3xl font-bold text-fuchsia-700 text-left'>Python</h1>
+               <div className='flex'>
+                <div className='text-left py-3'>
+                    <span><i class="fa-regular fa-clock text-blue-500"></i> 38 hours</span> < br/>
+                    <span><i class="fa-solid fa-video text-green-600"></i> 254 Lectures</span>
+                </div>
+                <div className='mx-auto'>
+                    <img className='w-24' src={img} alt='logo' />
+                </div>
+               </div>
+               <button className="btn btn-outline rounded-none lg:mr-5 bg-violet-800 font-bold text-white bg-gradient-to-r from-violet-300 to-violet-700 rounded w-full">View Course</button>
+            </div>
+
+
+        </div>
     );
 };
 
